088f36384bec1f86f8472feb4913337febc3c8bc
Paul Popescu <paul.popescu2003@gmail.com>
2025-01-05 22:25:17 +0200

    finished with comments

--- 6_1097_POPESCU_Paul.js
+++ 6_1097_POPESCU_Paul.js
@@ -1,4 +1,3 @@
-//https://docs.google.com/document/d/1UxLn1d8PxYq640Hhi1kxoce95qm6pNivzBVa99hWNWE/edit?tab=t.0
 const buttons = document.querySelectorAll(".tool-button");
 const svgCanvas = document.getElementById("svg-canvas");
 const lineWidthControl = document.getElementById("line-width");
@@ -10,48 +9,52 @@ let activeElement = null;
 let startX, startY, currentShape = null;
 let currentLineWidth = lineWidthControl.value;
 let currentLineColor = lineColorControl.value;
+let stack = []//stiva operatiilor pentru undo
 
-function autoSave() {
+
+function autoSave() {//salveaza istoricul modificarilor(stack-ul) facute asupra canvasului,
+    //daca e un element creat salveaza direct outerHTML altfel salveaza un obiect care contine si boolean deleted = true
     const stackArray = Array.from(stack).map(elem => elem.deleted ? { deleted: true, element: elem.element.outerHTML } : elem.outerHTML);
     localStorage.setItem("stack", JSON.stringify(stackArray));
 }
 
 function autoLoad() {
-    const stringuri = localStorage.getItem("stack");
+    const stringuri = localStorage.getItem("stack");//localstorage salveaza doar stringuri
     if (stringuri) {
-        const svgStringArray = JSON.parse(stringuri);
-        const parser = new DOMParser();
+        const svgStringArray = JSON.parse(stringuri);//le parsam JSON
+        const parser = new DOMParser();//ne trebuie ca sa cream elementele din string
 
         svgStringArray.forEach(obj => {
-            const str = obj.deleted ? obj.element : obj;
-            const svgElement = parser.parseFromString(str, "image/svg+xml").documentElement;
-            const newElement = document.createElementNS("http://www.w3.org/2000/svg", svgElement.tagName);
+            const str = obj.deleted ? obj.element : obj;//in str luam doar outerHTML-ul fara acel boolean
+            const svgElement = parser.parseFromString(str, "image/svg+xml").documentElement;//transformare in element svg
+            const newElement = document.createElementNS("http://www.w3.org/2000/svg", svgElement.tagName);//creare element
             Array.from(svgElement.attributes).forEach(attr => {
-                newElement.setAttribute(attr.name, attr.value);
+                newElement.setAttribute(attr.name, attr.value);//copiere atribute din svgElement in newElement
             });
 
             const toStack = obj.deleted ? { deleted: true, element: newElement } : newElement;
-            stack.push(toStack);
+            stack.push(toStack);//punem elementele in stack, cu deleted daca e vorba de o stergere
         });
     }
-    const deletedStack = stack.filter(item => item.deleted === true);
-    const nonDeletedStack = stack.filter(item => !item.deleted)
+    const deletedStack = stack.filter(item => item.deleted === true);//stergerile din stack
+    const nonDeletedStack = stack.filter(item => !item.deleted)//adaugarile din stack
     nonDeletedStack.forEach(elem => {
         if (deletedStack.findIndex(item => item.element.outerHTML === elem.outerHTML) === -1) {
-            svgCanvas.appendChild(elem);
+            svgCanvas.appendChild(elem);//adaugam doar ce nu a fost si sters ulterior
         }
     })
 }
 
+//la incarcarea DOM-ului
 document.addEventListener("DOMContentLoaded", () => {
-    autoLoad()
+    autoLoad();
 
     //pe svg canvas:
     svgCanvas.addEventListener("mousedown", onMouseDown);//cand dai click undeva
     svgCanvas.addEventListener("mousemove", onMouseMove);//cand misti mouse-ul
     svgCanvas.addEventListener("mouseup", onMouseUp);//cand ridici click-ul (termini de desenat)
 
-    undoButton.addEventListener("click", undoLast);
+    undoButton.addEventListener("click", undoLast);//functie undoLast la apasare undoButton
 
     // Actualizare grosime linie
     lineWidthControl.addEventListener("change", (e) => {
@@ -74,10 +77,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
             //facem butonul delete vizibil daca selectorul e activ
             activeTool === "selector" ? deleteButton.style.display = "inline" : deleteButton.style.display = "none";
+
+            //sa arate altfel cursorul la hover pe svgCanvas, cand e un tool altul decat selector activ
             updateCursorStyle();
         });
     });
 
+    //functionalitate buton delete
     deleteButton.addEventListener("click", onDeleteElement);
 
     //export SVG
@@ -92,35 +98,35 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function onDeleteElement() {
-    if (svgCanvas.contains(activeElement) && activeElement != svgCanvas) {
-        stack.push({ deleted: true, element: activeElement });
-        svgCanvas.removeChild(activeElement);
+    if (svgCanvas.contains(activeElement) && activeElement != svgCanvas) {//verificam sa nu fie chiar svgCanvas elementul activ
+        stack.push({ deleted: true, element: activeElement });//actualizam stiva cu elementul sters in caz ca vrem sa il recuperam cu undo
+        svgCanvas.removeChild(activeElement);//il stergem din canvas
 
         activeElement = null;
     }
-    autoSave();
+    autoSave();//salvam modificarile in localStorage
 }
 
-let stack = []//stiva operatiilor pentru undo
 function undoLast() {
     if (stack.length > 0) {
         const lastChange = stack.pop();
-        if (lastChange.deleted === true) {
+        if (lastChange.deleted === true) {//daca ultima schimbare a fost o stergere
             svgCanvas.appendChild(lastChange.element);
         }
-        else {
+        else {//daca ultima schimbare a fost o adaugare in canvas
             let toDelete = null;
-            svgCanvas.childNodes.forEach(child => {
-                if(child.outerHTML === lastChange.outerHTML) {
+            svgCanvas.childNodes.forEach(child => {//cautam copilul svgCanvas care e identic cu ce am scos din stiva
+                if (child.outerHTML === lastChange.outerHTML) {
                     toDelete = child;
                 }
             });
-            if(toDelete) svgCanvas.removeChild(toDelete);
+            if (toDelete) svgCanvas.removeChild(toDelete);
         }
-        autoSave();
+        autoSave();//salvam in localStorage
     }
 }
 
+//functie pentru a evita sa scriu de 3 ori acelasi cod
 function setTransparentAndStrokeOnCreate(shape) {
     shape.setAttribute("fill", "transparent");
     shape.setAttribute("stroke", currentLineColor);
@@ -208,26 +214,46 @@ function updateCursorStyle() {
 }
 
 function onMouseDown(e) {
-    if (activeTool) {//daca e tool selectat
-        if (shapeHandlers[activeTool]) {//daca e tool de desenat selectat(shapeHandlers contine toate toolurile de desenat)
+    if (activeTool) {//daca avem tool activ, altfel nu are rost eventul
+        if (shapeHandlers[activeTool]) {//daca incercam sa desenam una din cele 4 forme predefinite in shapeHandlers
             startX = e.offsetX;
-            startY = e.offsetY;
+            startY = e.offsetY;//puncte de start
 
             currentShape = shapeHandlers[activeTool].create(startX, startY);
             svgCanvas.appendChild(currentShape);
-            stack.push(currentShape);
-        }
-        else {//selector
+            stack.push(currentShape);//apelam functia de creare, o atasam la svgCanvas si o adaugam in stiva
+        } else {//daca tool-ul activ este selector(mutam elementul)
             activeElement = e.target;
-            console.log(activeElement);
-            if (svgCanvas.contains(activeElement) && activeElement !== svgCanvas) {
-                let initialX = parseFloat(activeElement.getAttribute("x") || activeElement.getAttribute("cx") || 0);
-                let initialY = parseFloat(activeElement.getAttribute("y") || activeElement.getAttribute("cy") || 0);
 
+            if (svgCanvas.contains(activeElement) && activeElement !== svgCanvas) {//daca elementul selectat nu e canvas-ul
+                let initialX, initialY, initialX1, initialY1, initialX2, initialY2;
+
+                if (activeElement.tagName === "rect") {//atribute necesare pt dreptunghi
+                    initialX = parseFloat(activeElement.getAttribute("x"));
+                    initialY = parseFloat(activeElement.getAttribute("y"));
+                    //dreptunghiul e definit de x/y colt, lungime si latime, nu avem nevoie aici
+                } else if (activeElement.tagName === "circle" || activeElement.tagName === "ellipse") {
+                    initialX = parseFloat(activeElement.getAttribute("cx"));//atribute necesare pt cerc/elipsa
+                    initialY = parseFloat(activeElement.getAttribute("cy"));
+                    //cercul si elipsa sunt definite de x/y centru, respectiv raza in cazul cercului, nu avem nevoie aici
+                } else if (activeElement.tagName === "line") {//atribute necesare pt linie
+                    initialX1 = parseFloat(activeElement.getAttribute("x1"));
+                    initialY1 = parseFloat(activeElement.getAttribute("y1"));
+                    initialX2 = parseFloat(activeElement.getAttribute("x2"));
+                    initialY2 = parseFloat(activeElement.getAttribute("y2"));
+                    //linia e definita de 2 puncte
+                }
+
+                const startMouseX = e.offsetX;
+                const startMouseY = e.offsetY;//punct de plecare mouse
+
+                // functie declansata cand se misca mouse-ul
                 const onMouseMove = (moveEvent) => {
-                    const dx = moveEvent.offsetX - startX;
-                    const dy = moveEvent.offsetY - startY;
+                    //distante parcurse de mouse pe x si y
+                    const dx = moveEvent.offsetX - startMouseX;
+                    const dy = moveEvent.offsetY - startMouseY;
 
+                    //modificari in element prin adaugarea distantei parcurse 
                     if (activeElement.tagName === "rect") {
                         activeElement.setAttribute("x", initialX + dx);
                         activeElement.setAttribute("y", initialY + dy);
@@ -235,44 +261,40 @@ function onMouseDown(e) {
                         activeElement.setAttribute("cx", initialX + dx);
                         activeElement.setAttribute("cy", initialY + dy);
                     } else if (activeElement.tagName === "line") {
-                        const x1 = parseFloat(activeElement.getAttribute("x1"));
-                        const y1 = parseFloat(activeElement.getAttribute("y1"));
-                        const x2 = parseFloat(activeElement.getAttribute("x2"));
-                        const y2 = parseFloat(activeElement.getAttribute("y2"));
+                        activeElement.setAttribute("x1", initialX1 + dx);
+                        activeElement.setAttribute("y1", initialY1 + dy);
+                        activeElement.setAttribute("x2", initialX2 + dx);
+                        activeElement.setAttribute("y2", initialY2 + dy);
+                    }
+                };
 
-                        activeElement.setAttribute("x1", x1 + dx);
-                        activeElement.setAttribute("y1", y1 + dy);
-                        activeElement.setAttribute("x2", x2 + dx);
-                        activeElement.setAttribute("y2", y2 + dy);
-                    };
-                }
+                //inlaturam evenimentele de move si up cand ridicam click-ul
                 const onMouseUp = () => {
-                    // Dezactivează evenimentele
                     svgCanvas.removeEventListener("mousemove", onMouseMove);
                     svgCanvas.removeEventListener("mouseup", onMouseUp);
                 };
 
-                // Adaugă evenimentele de mousemove și mouseup
+                //adaugam evenimentele de move si up cand apasam click-ul
                 svgCanvas.addEventListener("mousemove", onMouseMove);
                 svgCanvas.addEventListener("mouseup", onMouseUp);
 
-                // Actualizează coordonatele punctului de start
                 startX = e.offsetX;
                 startY = e.offsetY;
-
             }
         }
     }
 }
 
+//cand misti mouse-ul
 function onMouseMove(e) {
-    if (currentShape && activeTool && shapeHandlers[activeTool]) {
+    if (currentShape && activeTool && shapeHandlers[activeTool]) {//daca avem 
         const currentX = e.offsetX;
         const currentY = e.offsetY;
         shapeHandlers[activeTool].update(currentShape, currentX, currentY);
     }
 }
 
+//cand ridici click-ul
 function onMouseUp() {
     currentShape = null;
     autoSave();
